c8b1991713d0e1e369ce8374ff231d46a5942faf
GregorySD1707 <159218483+GregorySD1707@users.noreply.github.com>
2025-10-01 21:48:53 -0500

    Agregando atributos, getters y setters en modelo ResumenDiario

--- src/modelos/ResumenDiario.java
+++ src/modelos/ResumenDiario.java
@@ -1,3 +1,42 @@
 public class ResumenDiario {
+    private double ingresosTotales;
+    private double gastosTotales;
+    private double ahorroNeto;
+
+    public ResumenDiario() {
+        this.ingresosTotales = 0.0;
+        this.gastosTotales = 0.0;
+        this.ahorroNeto = 0.0;
+    }
+
+    public ResumenDiario(double ingresosTotales, double gastosTotales, double ahorroNeto) {
+        this.ingresosTotales = ingresosTotales;
+        this.gastosTotales = gastosTotales;
+        this.ahorroNeto = ahorroNeto;
+    }
+
+    public double getIngresosTotales() {
+        return ingresosTotales;
+    }
+
+    public double getGastosTotales() {
+        return gastosTotales;
+    }
+
+    public double getAhorroNeto() {
+        return ahorroNeto;
+    }
+
+    public void setIngresosTotales(double ingresosTotales) {
+        this.ingresosTotales = ingresosTotales;
+    }
+
+    public void setGastosTotales(double gastosTotales) {
+        this.gastosTotales = gastosTotales;
+    }
+
+    public void setAhorroNeto(double ahorroNeto) {
+        this.ahorroNeto = ahorroNeto;
+    }
 
 }
