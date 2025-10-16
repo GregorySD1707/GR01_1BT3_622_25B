a5d48beae45c4522a7489170361a1a17137acd08
GregorySD1707 <159218483+GregorySD1707@users.noreply.github.com>
2025-10-15 21:19:21 -0500

    Test Unitario assertEquals sign up de Usuario

--- src/test/java/com/sistema_financiero_personal/UsuarioTest.java
+++ src/test/java/com/sistema_financiero_personal/UsuarioTest.java
@@ -1,26 +1,25 @@
 package com.sistema_financiero_personal;
 
+import com.sistema_financiero_personal.movimiento.modelos.Cartera;
 import com.sistema_financiero_personal.usuario.daos.DAOUsuario;
 import com.sistema_financiero_personal.usuario.modelos.Usuario;
 import org.junit.Test;
-
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 import static org.junit.Assert.*;
 
 public class UsuarioTest {
 
     @Test
-    public void given_data_when_login_then_ok() {
-
+    public void given_data_when_signup_then_ok() {
         Usuario usuario = new Usuario("Pepe", "Zambrano", "pepe.zambrano@gmail.com",
                 "xXXpepeXxx", "abc123", LocalDate.parse("2002-11-27"));
         DAOUsuario daoUsuario = new DAOUsuario();
 
+        usuario.setCartera(new Cartera());
         daoUsuario.crear(usuario);
 
-        assertEquals("Pepe", usuario.getNombreUsuario());
+        assertEquals(usuario.getNombreUsuario(), daoUsuario.buscarPorId(usuario.getId()).getNombreUsuario());
     }
-
 }
+
+
