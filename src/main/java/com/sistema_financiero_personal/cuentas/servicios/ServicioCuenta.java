4a88c21abe25cbf6ba8137679f2dd7417eff2123
GregorySD1707 <159218483+GregorySD1707@users.noreply.github.com>
2025-10-25 12:52:36 -0500

    Agregando logica para manejar cuentas y creacion

--- src/main/java/com/sistema_financiero_personal/cuentas/servicios/ServicioCuenta.java
+++ src/main/java/com/sistema_financiero_personal/cuentas/servicios/ServicioCuenta.java
@@ -1,5 +1,16 @@
 package com.sistema_financiero_personal.cuentas.servicios;
 
-public class ServicioCuenta {
+import com.sistema_financiero_personal.cuentas.daos.DAOCuenta;
+import com.sistema_financiero_personal.cuentas.modelos.Cuenta;
 
+public class ServicioCuenta {
+    DAOCuenta daoCuenta = new DAOCuenta();
+
+    public void crearCuenta(Cuenta cuenta) {
+        daoCuenta.crear(cuenta);
+    }
+
+    public Object buscarCuenta(Long id) {
+        return daoCuenta.buscarPorId(id);
+    }
 }
