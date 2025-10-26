0cd60883d4c095a63f07f8b675cfa029caa49ece
GregorySD1707 <159218483+GregorySD1707@users.noreply.github.com>
2025-10-25 20:10:52 -0500

    Prueba de IC en Jenkins para crear imagen docker en nuevo repositorio

--- src/main/java/com/sistema_financiero_personal/resumen_financiero/modelos/ResumenFinanciero.java
+++ src/main/java/com/sistema_financiero_personal/resumen_financiero/modelos/ResumenFinanciero.java
@@ -48,6 +48,7 @@ public class ResumenFinanciero {
         fechaPeriodoActual = null;
         documentoPDF = null;
         this.fechaCreacion = LocalDateTime.now();
+        System.out.println("Prueba Docker");
     }
 
     public ResumenFinanciero(Double ingresosTotales, double gastosTotales, LocalDate fechaPeriodoAnterior, LocalDate fechaPeriodoActual, DocumentoPDF documentoPDF) {
