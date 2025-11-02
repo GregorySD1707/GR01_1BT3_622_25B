b4723f98ed8b34575b4061440acc7b6a265de64d
GregorySD1707 <159218483+GregorySD1707@users.noreply.github.com>
2025-11-02 13:02:19 -0500

    Manejo de nombre vacio en plantilla usando TDD

--- src/main/java/com/sistema_financiero_personal/plantillas/servicios/ServicioPlantilla.java
+++ src/main/java/com/sistema_financiero_personal/plantillas/servicios/ServicioPlantilla.java
@@ -1,4 +1,16 @@
 package com.sistema_financiero_personal.plantillas.servicios;
 
+import com.sistema_financiero_personal.cuentas.modelos.Cuenta;
+import com.sistema_financiero_personal.plantillas.modelos.Plantilla;
+import com.sistema_financiero_personal.usuario.modelos.Usuario;
+
 public class ServicioPlantilla {
+    public void crearPlantilla(Plantilla plantilla, Long usuario_id) {
+
+        boolean estaVacio = plantilla.getNombre().isEmpty();
+        boolean estaEnBlanco = plantilla.getNombre().isBlank();
+        if(estaVacio ||estaEnBlanco){
+            throw new IllegalArgumentException("El nombre no puede estar vacío");
+        }
+    }
 }
